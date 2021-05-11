c0e3dfe47cc50248ea6204c47f4ac90f4f1bb235
hypothe <markeagle98@gmail.com>
2021-05-11 17:28:18 +0000

    suppress exceptions

--- scripts/user_interface.py
+++ scripts/user_interface.py
@@ -18,14 +18,16 @@ def main():
             try:
                 ui_client("start")
             except rospy.ServiceException as e:
-                print("Service call to /user_interface failed with {}".format(e))
+                #print("Service call to /user_interface failed with {}".format(e))
+                pass    #silence the exception
             x = int(input("\nPress 0 to stop the robot "))
         else:
             print("Please wait, the robot is going to stop when the position will be reached")
             try:
                 ui_client("stop")
             except rospy.ServiceException as e:
-                print("Service call to /user_interface failed with {}".format(e))
+                pass
+                #print("Service call to /user_interface failed with {}".format(e))
             x = int(input("\nPress 1 to start the robot "))
             
 if __name__ == '__main__':
